bb983e18a3ecb2f3eec2f863e347f43aa8935081
HHN0609 <1446909703@qq.com>
2022-07-22 15:07:11 +0800

    1.优化冒泡排序

--- bubbleSort/bubbleSort.c
+++ bubbleSort/bubbleSort.c
@@ -6,7 +6,7 @@ void bubbleSort(int *arr, int length);
 void output(int *arr, int length);
 
 int main () {
-    int arr[ARR_SIZE] = {3, 4, -1, 9, 20, -10};
+    int arr[ARR_SIZE] = {1, 2, 3, 0, 5, 6};
     bubbleSort(arr, ARR_SIZE);
     output(arr, ARR_SIZE);
     return 0;
@@ -20,13 +20,17 @@ void output(int *arr, int length){
 }
 
 void bubbleSort(int *arr, int length){
+    int has_Swap = 0;
     for (int i = length-1; i>=1; i--){
         for(int j=0; j<i; j++){
             if(arr[j] > arr[j+1]){
+                has_Swap = 1;
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
             }
         }
+        if(!has_Swap) return;
+        has_Swap = 0;
     }
 }
